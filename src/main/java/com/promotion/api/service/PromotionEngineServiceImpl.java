151039458fc7a9063154b5258d3dec2b062afccc
Antonio <Antonio@DESKTOP-RSU7RPR.home>
2021-06-22 23:28:31 +0100

    Fix bug with not found promotion in order

--- src/main/java/com/promotion/api/service/PromotionEngineServiceImpl.java
+++ src/main/java/com/promotion/api/service/PromotionEngineServiceImpl.java
@@ -24,7 +24,7 @@ public class PromotionEngineServiceImpl implements PromotionEngineService {
 	//List with current promotions
 	private Promotions promotions;
 	private Catalog catalog;
-	
+
 	private Map<String, Long> products;
 
 	public PromotionEngineServiceImpl() {
@@ -33,14 +33,14 @@ public class PromotionEngineServiceImpl implements PromotionEngineService {
 			File file = ResourceUtils.getFile("classpath:promotions.json");
 			ObjectMapper mapper = new ObjectMapper();
 			promotions = mapper.readValue(file,  Promotions.class);
-			
+
 			//Read available products
 			file = ResourceUtils.getFile("classpath:catalog.json");
 			catalog = mapper.readValue(file,  Catalog.class);
-		
+
 			//Prepare map with products
 			products =  catalog.getProducts().stream().collect(Collectors.toMap(Product::getId, Product::getPrice, (a, b) -> b));
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
@@ -52,53 +52,54 @@ public class PromotionEngineServiceImpl implements PromotionEngineService {
 	public OrderResponse purchaseOrder(OrderRequest orderRequest) {
 
 		OrderResponse orderResponse = new OrderResponse();
-		
+
 		long total = 0;
-		
+
 		products =  catalog.getProducts().stream().collect(Collectors.toMap(Product::getId, Product::getPrice, (a, b) -> b));
-		
+
 		Map<String, Integer> purchasedProducts = new HashMap<>();
-			
+
 		//Process the orders. Lets calculate how many items are per product
 		for (UnitOrder unitOrder:orderRequest.getOrders()) {
 			Integer sum = purchasedProducts.getOrDefault(unitOrder.getIdProduct(), 0);
 			sum+=unitOrder.getQuantity();
 			purchasedProducts.put(unitOrder.getIdProduct(), sum);
 		}
-		
+
 		Map<String, Integer> promotionsFoundInOrder = new HashMap<>();
 		//Check promotions
 		for (UnitPromotion unitPromotion:promotions.getPromotions()) {
-			
+
 			int currentPromotion = 0;
-			
+
 			for (UnitOrder unitOrder: unitPromotion.getUnitOrder()) {
-				
+
 				//Get the current catalog of promotions. The promotion could contain more than one product
 				Integer numberOfProducts = purchasedProducts.getOrDefault(unitOrder.getIdProduct(), 0);
-				
+
 				if (numberOfProducts==0 || numberOfProducts<unitOrder.getQuantity()) {
 					//No enough products for this promotion
+					promotionsFoundInOrder.put(unitPromotion.getId(),-1);
 					break;
 				}
-				
+
 				int promotionsFound = promotionsFoundInOrder.getOrDefault(unitPromotion.getId(), -1);
-				
+
 				currentPromotion = numberOfProducts / unitOrder.getQuantity();
-				
+
 				if (promotionsFound==-1||promotionsFound>currentPromotion) {
 					promotionsFoundInOrder.put(unitPromotion.getId(), currentPromotion);
 				}
-				
+
 			}
-			
+
 			currentPromotion = promotionsFoundInOrder.getOrDefault(unitPromotion.getId(), -1) ;
-			
+
 			if (currentPromotion > 0) {
-				
+
 				//Lets Calculate partial total
 				total += (unitPromotion.getPromotionPrice()*currentPromotion);
-				
+
 				//Update map with products from order pending to process
 				for (UnitOrder unitOrder: unitPromotion.getUnitOrder()) {
 					Integer numberOfProducts = purchasedProducts.get(unitOrder.getIdProduct());
@@ -108,11 +109,11 @@ public class PromotionEngineServiceImpl implements PromotionEngineService {
 					numberOfProducts -= (unitOrder.getQuantity()*currentPromotion);
 					purchasedProducts.put(unitOrder.getIdProduct(), numberOfProducts);
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		//Check the remaining number of products of the order and calculate total order
 		Iterator<String> productKeys  = purchasedProducts.keySet().iterator();
 		while (productKeys.hasNext()) {
@@ -120,9 +121,9 @@ public class PromotionEngineServiceImpl implements PromotionEngineService {
 			long price = products.get(key);
 			total+=price*(purchasedProducts.get(key));
 		}
-		
+
 		orderResponse.setTotal(total);
-		
+
 		return orderResponse;
 	}
 
